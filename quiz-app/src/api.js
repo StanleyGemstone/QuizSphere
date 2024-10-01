a1f7441476a7c16d2689397bb2161bb3ed190a5c
StanleyGemstone <stanleynduchi@gmail.com>
2024-10-01 05:45:49 +0100

    not working yet but still fresh code2

--- quiz-app/src/api.js
+++ quiz-app/src/api.js
@@ -1,15 +1,15 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '(https://opentdb.com/api.php?amount=20&category=19&difficulty=medium)',
+  baseURL: '(https://opentdb.com/api.php?amount=10&category=19&difficulty=easy&type=multiple)',
 });
 
 export const fetchQuestions = async () => {
   const response = await api.get('api.php', {
     params: {
-      amount: 20,
-      category: 19,
-      difficulty: 'medium',
+      amount: 10,
+      category: 9,
+      difficulty: 'easy',
       type: 'multiple',
     },
   });
